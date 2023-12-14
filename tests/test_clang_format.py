75edf9e3dd67235f6f0f703c13ac20cb3ffcd3bf
Tom de Geus <tdegeus@users.noreply.github.com>
2023-12-14 17:14:00 +0100

    Using `pytest` (#10)

--- tests/test_clang_format.py
+++ tests/test_clang_format.py
@@ -1,45 +1,36 @@
-import unittest
-
 import cpp_comment_format
 
 
-class TestClangFormat(unittest.TestCase):
-    """ """
+def test_Docstrings_basic():
+    code = """
+/**
+* This is the global docstring.
+* @code{.cpp}
+* int a=0;
+* @endcode
+*/
 
-    def test_Docstrings_basic(self):
-        code = """
-        /**
-        * This is the global docstring.
-        * @code{.cpp}
-        * int a=0;
-        * @endcode
-        */
+/**
+ * My first function.
+ * @param a This is a parameter.
+ */
+int foo(int a);
+"""
 
-        /**
-         * My first function.
-         * @param a This is a parameter.
-         */
-        int foo(int a);
-        """
+    formatted = """
+/**
+* This is the global docstring.
+* @code{.cpp}
+* int a = 0;
+* @endcode
+*/
 
-        formatted = """
-        /**
-        * This is the global docstring.
-        * @code{.cpp}
-        * int a = 0;
-        * @endcode
-        */
+/**
+ * My first function.
+ * @param a This is a parameter.
+ */
+int foo(int a);
+"""
 
-        /**
-         * My first function.
-         * @param a This is a parameter.
-         */
-        int foo(int a);
-        """
-
-        ret = cpp_comment_format.clang_format(code)
-        self.assertEqual(formatted.strip(), ret.strip())
-
-
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    ret = cpp_comment_format.clang_format(code)
+    assert formatted.strip() == ret.strip()
